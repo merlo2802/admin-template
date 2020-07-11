73c41d28308ee9b1930c9b0263648e99648f27bb
Alvaro Merlo <alvaromerlo2802@gmail.com>
2020-07-11 16:28:45 -0400

    pages-module progress component

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,8 +4,8 @@ import { SharedModule } from '../shared/shared.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { ProgressComponent } from './progress/progress.component';
-import { PagesComponent } from './pages.component';
 import { Pages_Routes } from './pages.routing';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -19,6 +19,7 @@ import { Pages_Routes } from './pages.routing';
     CommonModule,
     Pages_Routes,
     SharedModule,
+    FormsModule,
   ],
   exports:[
     DashboardComponent,
